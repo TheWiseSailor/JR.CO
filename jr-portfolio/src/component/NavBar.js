683b95e54fc48bc509cd79e21f7597772250e418
TheWiseSailor <rudasilljay@gmail.com>
2023-09-04 14:29:08 -0400

    1.04 added social links

--- jr-portfolio/src/component/NavBar.js
+++ jr-portfolio/src/component/NavBar.js
@@ -1,4 +1,34 @@
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import logo from "../assets/img/logo.svg";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+import { HashLink } from "react-router-hash-link";
+import { BrowserRouter as Router } from "react-router-dom";
+
 export const NavBar = () => {
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 50) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
+
   return (
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
@@ -43,19 +73,25 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#">
-                  <img src={navIcon1} alt="" />
+                <a href="https://discordapp.com/users/389945057345667077">
+                  <img src={navIcon1} alt="Discord" />
                 </a>
-                <a href="#">
-                  <img src={navIcon2} alt="" />
+                <a href="https://github.com/TheWiseSailor">
+                  <img src={navIcon1} alt="Github" />
                 </a>
-                <a href="#">
-                  <img src={navIcon3} alt="" />
+                <a href="https://www.linkedin.com/in/joseph-rudasill-2565a41ba/">
+                  <img src={navIcon1} alt="Linked-In" />
+                </a>
+                <a href="https://www.snapchat.com/add/asianstudmuffin?share_id=gTwL0A4CTtWL7ns+pfxyvg&locale=en_US">
+                  <img src={navIcon2} alt="Snapchat" />
+                </a>
+                <a href="https://www.instagram.com/joseph_rudasill_">
+                  <img src={navIcon3} alt="Instagram" />
                 </a>
               </div>
               <HashLink to="#connect">
                 <button className="vvd">
-                  <span>Get in Touch</span>
+                  <span>Connect!</span>
                 </button>
               </HashLink>
             </span>
