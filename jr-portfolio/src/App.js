27ff24b254f6aafe16e32c88b3dc55e57d8a5efa
TheWiseSailor <rudasilljay@gmail.com>
2023-09-06 12:38:27 -0400

    v1.22 routes fixed but nothing still wants to load

--- jr-portfolio/src/App.js
+++ jr-portfolio/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.png";
 import "./App.css";
-
+import "./components/About.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
 function App() {
