9f7361188a5a78ef98ee580d1f5919dc226d316b
TheWiseSailor <rudasilljay@gmail.com>
2023-09-06 12:52:51 -0400

    1.24 another simple debug @Donna for helping me debug,will mention her in sources

--- jr-portfolio/src/App.js
+++ jr-portfolio/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import "./components/About.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { NavBar } from "./components/NavBar";
+import { About } from "./components/About.js";
 function App() {
   return (
     <div className="App">
