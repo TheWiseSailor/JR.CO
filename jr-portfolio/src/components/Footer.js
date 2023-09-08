a176246a027d2c75fe74c0d36c771b3bee3998bf
TheWiseSailor <rudasilljay@gmail.com>
2023-09-08 12:11:57 -0400

    v1.42 simple footer made

--- jr-portfolio/src/components/Footer.js
+++ jr-portfolio/src/components/Footer.js
@@ -1,24 +1,11 @@
-import logo from "../logo.png";
-import discord from "../assets/img/discord.png";
-import github from "../assets/img/github.png";
-import linkedin from "../assets/img/linkedin.png";
-import snapchat from "../assets/img/snapchat.png";
-import instagram from "../assets/img/instagram.png";
 export const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-contact-info">
-        <h5>Joseph Rudasill</h5>
-        <h6>Frontend Developer</h6>
+      <div className="contact">
+        Contact Me! |
+        <a href="mailto:Rudasilljay@gmail.com"> Rudasilljay@gmail.com</a>
+        <div className="copyright">&copy; JR.CO | All Rights Reserved</div>
       </div>
-      <div className="footer-contact-info">
-        <h5>Connect:</h5>
-        <h6>
-          <a href="mailto: rudasilljay@gmail.com">rudasilljay@gmail.com</a>
-        </h6>
-      </div>
-      <div className="footer-social-icon">{/* <h4>Let's Connect:</h4> */}</div>
-      <p>Copyright 2023. All Rights Reserved</p>
     </footer>
   );
 };
