a37159dedcfac92a32f475133ff89eed1aabf452
TheWiseSailor <rudasilljay@gmail.com>
2023-09-10 18:39:03 -0400

    1.69 css changes

--- jr-portfolio/src/components/pages/ProjectDetails.js
+++ jr-portfolio/src/components/pages/ProjectDetails.js
@@ -11,18 +11,27 @@ export const ProjectDetails = ({
     <div className="col-lg-4 col-md-6 col-sm-12">
       <div className="project-card">
         <img src={imgUrl} alt={title} className="rounded-image" />
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <div className="project-buttons button-spacing">
+        <h3>
+          <strong>{title}</strong>
+        </h3>
+        <p>
+          <strong>{description}</strong>
+        </p>
+        <div className="project-buttons ">
           {githubLink && (
             <a href={githubLink} target="_blank" rel="noopener noreferrer">
-              <button className="btn ">GitHub</button>
+              <button className="btn ">
+                {" "}
+                <strong>GitHub</strong>
+              </button>
             </a>
           )}
 
           {deployLink && (
             <a href={deployLink} target="_blank" rel="noopener noreferrer">
-              <button className="btn ">Live Demo</button>
+              <button className="btn ">
+                <strong>Live Demo</strong>
+              </button>
             </a>
           )}
         </div>
