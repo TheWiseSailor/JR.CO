dd789ebcfcf7dc38ca830dab6cf447b4996b1acb
TheWiseSailor <rudasilljay@gmail.com>
2023-09-11 15:24:50 -0400

    added comments for about.js

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -7,19 +7,24 @@ import "animate.css";
 import TrackVisibility from "react-on-screen";
 
 export const About = () => {
-  const [loopNum, setLoopNum] = useState(0);
-  const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState("");
-  const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
+  // Initialize state variables
+  const [loopNum, setLoopNum] = useState(0); // Counter for text rotation cycles
+  const [isDeleting, setIsDeleting] = useState(false); // Indicates if text is being deleted
+  const [text, setText] = useState(""); // The text being displayed and rotated
+  const [delta, setDelta] = useState(300 - Math.random() * 100); // Time delay between text changes
+  const [index, setIndex] = useState(1); // Index to track current text rotation
+
+  // Define an array for text rotation
   const toRotate = ["Web Developer", "Web Designer", "UI/UX Designer"];
-  const period = 2000;
+  const period = 2000; // Time period for each full text rotation cycle
 
   useEffect(() => {
+    // Set up a timer interval for the tick function
     let ticker = setInterval(() => {
       tick();
     }, delta);
 
+    // Clean up the timer when the component unmounts
     return () => {
       clearInterval(ticker);
     };
@@ -29,26 +34,26 @@ export const About = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
     let updatedText = isDeleting
-      ? fullText.substring(0, text.length - 1)
-      : fullText.substring(0, text.length + 1);
+      ? fullText.substring(0, text.length - 1) // Delete a character from the text
+      : fullText.substring(0, text.length + 1); // Add a character to the text
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta((prevDelta) => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2); // Halve the time delay when deleting
     }
 
     if (!isDeleting && updatedText === fullText) {
-      setIsDeleting(true);
-      setIndex((prevIndex) => prevIndex - 1);
-      setDelta(period);
+      setIsDeleting(true); // Start deleting when text is fully displayed
+      setIndex((prevIndex) => prevIndex - 1); // Decrement the index
+      setDelta(period); // Set the time delay to the rotation period
     } else if (isDeleting && updatedText === "") {
-      setIsDeleting(false);
-      setLoopNum(loopNum + 1);
-      setIndex(1);
-      setDelta(500);
+      setIsDeleting(false); // Stop deleting when text is empty
+      setLoopNum(loopNum + 1); // Increment the text rotation cycle
+      setIndex(1); // Reset the index
+      setDelta(500); // Set a shorter time delay
     } else {
-      setIndex((prevIndex) => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1); // Increment the index
     }
   };
 
@@ -64,6 +69,7 @@ export const About = () => {
                     isVisible ? "animate__animated animate__fadeIn" : ""
                   }
                 >
+                  {/* Render content */}
                   <span className="tagline">Welcome to JR.CO</span>
                   <h1>
                     {`Hello! I'm Joseph,`}{" "}
@@ -76,13 +82,7 @@ export const About = () => {
                     </span>
                   </h1>
                   <p>Striving For Excellence, The Best Has Yet To Come.</p>
-                  {/* <p>
-                    I hold an Associate's degree in Science (AS) and possess a a
-                    moderate grasp of frontend frameworks. My professional
-                    strengths include exceptional interpersonal skills and adept
-                    communication, facilitating productive collaborations and
-                    the timely accomplishment of goals.
-                  </p> */}
+
                   <img
                     className="profilePic"
                     src={profilePic}
@@ -103,6 +103,7 @@ export const About = () => {
                     isVisible ? "animate__animated animate__zoomIn" : ""
                   }
                 >
+                  {/* Render an image */}
                   <img className="headerImg" src={headerImg} alt="headerImg" />
                 </div>
               )}
