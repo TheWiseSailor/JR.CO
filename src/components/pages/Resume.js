a6465f65bc9ee148052f2bf245d2c8464a0d6474
TheWiseSailor <rudasilljay@gmail.com>
2023-10-03 15:43:09 -0400

    updated google drive link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,7 +4,7 @@ import resumePDF from "../../assets/img/Resume/Resume.pdf";
 
 export const Resume = () => {
   const onlineResumeLink =
-    "https://drive.google.com/file/d/18whwIqrtSMib8307ur40gqunxxt3zl5z/view?usp=drive_link";
+    "https://drive.google.com/file/d/1R6HWrgVKUqyz3WzfF5j1wk6WP_7YwX5j/view?usp=sharing";
 
   return (
     <section className="resume" id="resume">
