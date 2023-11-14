8ec9e840bb386ba266f6d5d8a723dcf91141b9e1
TheWiseSailor <rudasilljay@gmail.com>
2023-11-14 18:57:12 -0500

    logo made to home reroute

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ export const Header = ({ activeLink, onUpdateActiveLink }) => {
   return (
     <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
       <Container>
-        <Navbar.Brand href="/">
+        <Navbar.Brand href="/" onClick={() => onUpdateActiveLink("home")}>
           <img src={logo} alt="Logo" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav">
